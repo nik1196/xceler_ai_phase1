e82a22d9bfac016570484a5204fe85b60e716bf7
nik1196 <nikp11396@gmail.com>
2017-07-07 23:18:19 +0530

    adding server files

---
+++ server.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, jsonify, request
+import re
+import socket
+from stage1_calc3 import *
+
+app = Flask(__name__)
+@app.route('/', methods = ['GET', 'POST'])
+def input_process():
+        reply = {"reply":"Hello, how may i help you?"}
+        return jsonify(reply)
+@app.route('/input', methods = ['GET', 'POST'])
+def input_process2():
+        print(request)
+        query = request.json['input']
+        result = calc(query)
+        
+        print(result)
+        if result:
+                reply = {"reply":' '.join(str(i) for i in result)}
+                return jsonify(reply)
+        else:
+                reply = {"reply":"Invalid or malformed request, please try again."}
+                return jsonify(reply)
+if __name__ == '__main__':
+    port = 8000
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    x = s.getsockname()[0]
+    print(x)
+    app.run(host=x, port=port)
